6e29141ef168c5a31b2f0e76d0d59081596a98ad
Nekupaska <dan-rex@hotmail.es>
2017-05-02 13:43:55 +0200

    Update Main.java

--- src/daw_ed04/Main.java
+++ src/daw_ed04/Main.java
@@ -18,8 +18,6 @@ public class Main {
     public static void main(String[] args) {
         CCuenta a = new CCuenta("Felipe Segundo","2222-2222-22-1230456789", 2222,0);
         
-        //hola mundo
-        System.out.println("holamundo");
         
         
         System.out.println("Saldo actual: "+ a.getSaldo());
